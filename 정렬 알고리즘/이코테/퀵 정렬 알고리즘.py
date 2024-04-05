573c5bc703c213a60951b6470e785609fc74a11f
1ny0ung <0828_iyiy@naver.com>
2024-04-05 09:08:20 +0900

    [Fix] 240405 정렬 알고리즘 이코테 예제: 퀵정렬 알고리즘 수정

--- 정렬 알고리즘/이코테/퀵 정렬 알고리즘.py
+++ 정렬 알고리즘/이코테/퀵 정렬 알고리즘.py
@@ -32,7 +32,7 @@ def quick_sort(array):
     tail = array[1:] # 피벗을 제외한 리스트
 
     left_side = [x for x in tail if x <= pivot] # 분할된 왼쪽 부분
-    right_side = [x for x in tail if x >= pivot] # 분할된 오른쪽 부분
+    right_side = [x for x in tail if x > pivot] # 분할된 오른쪽 부분
 
     # 분할 이후, 왼쪽 부분과 오른쪽 부분에서 각각 정렬 수행하고, 전체 리스트 반환
     return quick_sort(left_side) + [pivot] + quick_sort(right_side)
